1ad212cd8e72fd5921b801d731fbcf485ff92039
ferosekhanj <ferosekhanjs@outlook.com>
2015-05-15 11:58:50 +0530

    Initial Commit

---
+++ OutlookCal/OutlookCalendar.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections.Generic;
+/*
+ * Created by SharpDevelop.
+ * User: ic003194
+ * Date: 2/6/2012
+ * Time: 5:12 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace OutlookCal
+{
+	using System;
+	using Microsoft.Office.Interop.Outlook;
+	
+	/// <summary>
+	/// Description of OutlookCalendar.
+	/// </summary>
+	public class OutlookCalendar
+	{
+		Application myApp;
+		
+		NameSpace myMapiNS;
+		MAPIFolder myCalendar;
+		
+		public OutlookCalendar()
+		{
+			myApp 		= new ApplicationClass();
+			myMapiNS 	= myApp.GetNamespace("MAPI");
+			myCalendar 	= myMapiNS.GetDefaultFolder(OlDefaultFolders.olFolderCalendar);
+		}
+		
+		public IList<ApptItem> QueryCalendarForTheDay(DateTime aToday)
+		{
+			List<ApptItem> anItems = new List<ApptItem>();
+			DateTime aTomorrow = aToday.Date.AddDays(1);
+			foreach( AppointmentItem anAppt in myCalendar.Items)
+			{
+                AppointmentItem currentApt = anAppt;
+				DateTime aStart =  anAppt.Start;
+                DateTime anEnd = anAppt.End;
+                if (!anAppt.IsRecurring)
+				{
+					if( anEnd < aToday || anEnd > aTomorrow || aStart > aTomorrow)
+					{
+						continue;
+					}
+				}
+				else if (!DoesOccur(anAppt,aToday,aTomorrow))
+				{
+					continue;	
+				}
+				
+				if( anAppt.IsRecurring)
+				{
+					if( (currentApt = GetOccurence(anAppt,aToday)) == null)
+					{
+						continue;
+					}
+				}
+				
+				anItems.Add(
+					new ApptItem() 
+					{
+                        Start = new DateTime(currentApt.Start.Ticks),
+                        End = new DateTime(currentApt.End.Ticks),
+                        Summary = currentApt.Subject,
+                        Location = currentApt.Location						
+					});
+			}
+			
+			return anItems;
+		}
+		
+		bool DoesOccur(AppointmentItem theAppt, DateTime theToday, DateTime theTomorrow)
+		{
+			RecurrencePattern anObjPattern = theAppt.GetRecurrencePattern();
+			if( anObjPattern.NoEndDate || anObjPattern.PatternEndDate > theToday)
+			{
+				return DoesOccurToday(theAppt, anObjPattern, theToday);
+			}
+			return false;
+		}
+		
+		bool DoesOccurToday(AppointmentItem theAppt, RecurrencePattern theObjPattern, DateTime theToday)
+		{
+			switch (theObjPattern.RecurrenceType) {
+				case OlRecurrenceType.olRecursDaily:
+					return true;
+				case OlRecurrenceType.olRecursWeekly:
+				case OlRecurrenceType.olRecursMonthly:
+					return DoesOccurInThisWeek(theAppt,theObjPattern,theToday);
+				default:
+					return false;
+			}
+		}
+		
+		bool DoesOccurInThisWeek(AppointmentItem theAppt, RecurrencePattern theObjPattern, DateTime theToday)
+		{
+			DateTime anOcc = new DateTime(theToday.Year, theToday.Month, theToday.Day,
+			                             theAppt.Start.Hour, theAppt.Start.Minute,theAppt.Start.Second);
+			
+			if ( ( (1 << ((int)theToday.DayOfWeek)) & ((int)theObjPattern.DayOfWeekMask) ) > 0)
+			{
+  			 	int offset = theAppt.Start.Day - theToday.Day;
+		        TimeSpan timeSpan = ( anOcc - theAppt.Start ); 
+		        return ( (timeSpan.TotalDays+offset / 7) % theObjPattern.Interval == 0 );
+			}
+			
+			return false;
+		}
+
+        AppointmentItem GetOccurence(AppointmentItem theAppt, DateTime theToday)
+		{
+			AppointmentItem apptOccu = null;
+			DateTime aDay = new DateTime(theToday.Year,theToday.Month,theToday.Day,
+			                             theAppt.Start.Hour-1,theAppt.Start.Minute,0);
+			for(int i = 0; i < 3; i++)
+			{
+				try
+				{
+					apptOccu = theAppt.GetRecurrencePattern().GetOccurrence(aDay);
+					break;
+				}
+				catch(System.Exception e)
+				{
+					aDay =aDay.AddHours(1);
+				}
+			}
+			return apptOccu;
+		}
+	}
+	
+	public class ApptItem
+	{
+		public DateTime Start{get;set;}
+		public DateTime End{get;set;}
+		public String Summary{get;set;}
+		public String Location {get;set;}
+	}
+}
---
+++ OutlookCal/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ic003194
+ * Date: 2/6/2012
+ * Time: 4:34 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using Microsoft.Office.Interop.Outlook;
+
+namespace OutlookCal
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+            DateTime aToday = DateTime.Now.Date.AddDays((DateTime.Now.Date.DayOfWeek==DayOfWeek.Friday)?3.0:1.0);
+            if (args.Length == 0)
+            {
+                Console.WriteLine("OutlookCal.exe <username@gmail.com>");
+                return;
+            }
+
+			OutlookCalendar aCal = new OutlookCalendar();
+			
+			IList<ApptItem> anItemsToArchive = aCal.QueryCalendarForTheDay(aToday);
+
+			if( anItemsToArchive.Count == 0 )
+			{
+				Console.WriteLine("Nothing to archive!!");
+				return;
+			}
+			Console.WriteLine("{0}\r\n==========",aToday);
+			
+			GoogleCalendar aCloudCal = new GoogleCalendar(args[0]);
+			aCloudCal.ListTodaysAppt();
+			
+			foreach(ApptItem anAppt in anItemsToArchive)
+			{
+				Console.WriteLine("{0} {1} {2} {3} {4}",aToday.ToShortDateString(),anAppt.Start.ToShortTimeString(),anAppt.End.ToShortTimeString(),anAppt.Summary,anAppt.Location);
+				aCloudCal.AddEvent(anAppt.Start,anAppt.End,anAppt.Summary,anAppt.Location);
+			}
+			
+			Console.Write("\r\nPress any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
