c722433d113cc74a5aa2adfc944e1f79efb554ff
ferosekhanj <ferosekhanjs@outlook.com>
2015-05-27 10:24:22 +0530

    Added a stub for the google secrets.

---
+++ OutlookCal/MyGoogleSecret.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OutlookCal
+{
+    class MyGoogleSecret
+    {
+        // Register with google as an app developer and get the access token for accessing calendar apis.
+        public static String ClientIdentifier = "<clientidfromgoogle>.apps.googleusercontent.com";
+        public static String ClientSecret = "<cleintkey>";
+    }
+}
